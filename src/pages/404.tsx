d4186c37ef23b42e1cbbbbb491511506fde8baf0
ponte-vecchio <zkogdxdkur@p.monash.edu>
2022-05-20 11:54:24 +1000

    make the cat readable
    Signed-off-by: ponte-vecchio <zkogdxdkur@p.monash.edu>

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,20 +3,20 @@ import type { InferGetStaticPropsType, NextPage } from "next";
 export default function Custom404() {
 	return (
 		<>
-		<body className="text-red">
+		<body className="bg-bg text-yellow text-center mt-0 text-2xl font-firacode">
 			{/* Monospace Font + No Gap Between Lines */}
-			<p className="text-center mt-0 text-1xl font-mono font-bold">,_⠀⠀⠀⠀⠀_⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</p>
-			<p className="text-center mt-0 text-1xl font-mono font-bold">⠀|\\_,-~/⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀!⠀404⠀!</p>
-			<p className="text-center mt-0 text-1xl font-mono font-bold">⠀/⠀ㅡ⠀⠀ㅡ⠀|⠀⠀⠀⠀,--.⠀⠀⠀⠀⠀/⠀⠀⠀⠀⠀⠀</p>
-			<p className="text-center mt-0 text-1xl font-mono font-bold">(⠀⠀.⠀⠀.⠀)⠀⠀⠀/⠀,-'⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</p>
-			<p className="text-center mt-0 text-1xl font-mono font-bold">⠀\⠀⠀_Y_/-._(⠀(⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</p>
-			<p className="text-center mt-0 text-1xl font-mono font-bold">⠀/⠀⠀⠀⠀⠀⠀⠀⠀⠀`.⠀\⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</p>
-			<p className="text-center mt-0 text-1xl font-mono font-bold">|⠀⠀⠀⠀⠀⠀⠀⠀⠀_⠀⠀\⠀|⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</p>
-			<p className="text-center mt-0 text-1xl font-mono font-bold">\⠀⠀\⠀,⠀⠀/⠀⠀⠀⠀⠀⠀|⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</p>
-			<p className="text-center mt-0 text-1xl font-mono font-bold">⠀|⠀|⠀|-_\__⠀⠀⠀/⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</p>
-			<p className="text-center mt-0 text-1xl font-mono font-bold">(⠀(_/`(____,-'⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</p>
+			<p>,_     _                   </p>
+			<p> |\\_,-~/             ! 404 !</p>
+			<p> / ㅡ  ㅡ |    ,--.     /      </p>
+			<p>(  @  @ )   / ,-'            </p>
+			<p> \  _Y_/-._( (               </p>
+			<p> /         `. \              </p>
+			<p>|         _  \ |             </p>
+			<p>\  \ ,  /      |             </p>
+			<p> | | |-_\__   /              </p>
+			<p>( (_/`(____,-'               </p>
 			<p className="text-center mt-20 text-3xl font-bold">
-				404 | Page not found.
+				Page not found.
 			</p>
 		</body>
 		</>
